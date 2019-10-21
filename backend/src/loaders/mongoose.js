14da92e9cc93acc5882e6920bd689ccd9de8659a
Cleiton Luiz <cleiton.vanquish@gmail.com>
2019-10-21 20:27:57 -0300

    Add console.log

--- backend/src/loaders/mongoose.js
+++ backend/src/loaders/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = async () => {
+  console.log(process.env.MONGO_URI);
   const connection = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
